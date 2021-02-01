66c89ca4c766dc6d41ea94e60500020cf9a585eb
Michael Irvine <michaelirvinedesign@gmail.com>
2021-02-01 15:15:02 -0800

    revision to responsive image snippet

--- assets/lazyImg.js
+++ assets/lazyImg.js
@@ -14,7 +14,11 @@ document.addEventListener('DOMContentLoaded', function () {
       entries.forEach(function (entry) {
         if (entry.intersectionRatio > 0) {
           let image = entry.target;
-          image.srcset = image.dataset.srcset;
+          // image.srcset = image.dataset.srcset;
+          image.srcset = image.dataset.srcset
+            ? image.dataset.srcset
+            : image.srcset;
+          console.log(image.srcset);
           image.classList.remove('lazy');
 
           imageObserver.unobserve(image);
